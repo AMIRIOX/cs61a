5bbdfd9c06c9e37554d08fadc2490cecd275fe0d
AMIRIOX <wumingyun2120@outlook.com>
2025-02-10 13:07:36 +0800

    [disc01] Control, Environment Diagrams

---
+++ disc01/Q4.py
@@ -0,0 +1,35 @@
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(101) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    f = 0
+    for i in range(0, 10):
+        if has_digit(n, i):
+            f += 1
+    return f
+
+def has_digit(n, k):
+    """Returns whether k is a digit in n.
+
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    assert k >= 0 and k < 10
+    "*** YOUR CODE HERE ***"
+    
+    while n > 0:
+        if n % 10 == k:
+            return True
+        n = n // 10;
+    return False
+
+
